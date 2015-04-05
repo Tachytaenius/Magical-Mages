cc4beb77e40fec883b751e99582264e2d43050d3
wolfboyft <henryfthomson@gmail.com>
2015-04-05 19:17:56 +0100

    This is truly quite difficult.
    Yeah, you think it works? I'm still trying to get Villager Tweaks
    compatibility!

--- main/java/wolfboyft/magicalmages/Reference.java
+++ main/java/wolfboyft/magicalmages/Reference.java
@@ -3,7 +3,7 @@ package wolfboyft.magicalmages;
 public class Reference {
 	public static final String MOD_ID = "magicalmages";
 	public static final String MOD_NAME = "Magical Mages";
-	public static final String VERSION = "1.4";
+	public static final String VERSION = "1.5";
 	public static final String CLIENT_PROXY_CLASS = "wolfboyft.magicalmages.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "wolfboyft.magicalmages.proxy.CommonProxy";
 }
